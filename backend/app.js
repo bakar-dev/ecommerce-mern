f0343f5ab420cf7d52a92e2889c4bc672ece46d8
muhib bakar <muhib.bakar@Cdocs.local>
2022-06-26 18:31:57 +0500

    Orders functionality

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
-import express from "express";
-import cookieParser from "cookie-parser";
+import express from 'express';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
@@ -7,14 +7,16 @@ app.use(express.json());
 app.use(cookieParser());
 
 //import routes
-import product from "./routes/productRoutes.js";
-import user from "./routes/userRoutes.js";
+import product from './routes/productRoutes.js';
+import user from './routes/userRoutes.js';
+import order from './routes/orderRoutes.js';
 
-app.use("/api/v1", product);
-app.use("/api/v1", user);
+app.use('/api/v1', product);
+app.use('/api/v1', user);
+app.use('/api/v1', order);
 
 //error middleware
-import errorMiddlewar from "./middlewares/error.js";
+import errorMiddlewar from './middlewares/error.js';
 
 app.use(errorMiddlewar);
 
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,23 +1,33 @@
-import express from "express";
+import express from 'express';
 import {
   createProduct,
+  createProductReview,
   deleteProduct,
+  deleteReview,
   getAllProducts,
   getProduct,
+  getProductReviews,
   updateProduct,
-} from "../controllers/productController.js";
-import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js";
+} from '../controllers/productController.js';
+import { authorizeRoles, isAuthenticatedUser } from '../middlewares/auth.js';
 
 const router = express.Router();
 
-router.route("/products").get(getAllProducts);
+router.route('/products').get(getAllProducts);
 router
-  .route("/product")
+  .route('/product')
   .post(isAuthenticatedUser, authorizeRoles, createProduct);
 router
-  .route("/product/:id")
+  .route('/product/:id')
   .get(getProduct)
   .put(isAuthenticatedUser, authorizeRoles, updateProduct)
   .delete(isAuthenticatedUser, authorizeRoles, deleteProduct);
+
+router.route('/review').put(isAuthenticatedUser, createProductReview);
+
+router
+  .route('/reviews')
+  .get(getProductReviews)
+  .delete(isAuthenticatedUser, deleteReview);
 
 export default router;
